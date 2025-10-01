ed59110b4c47962e43aa0515b23ca7507bd5ba0c
Procopi Constantinou <pcconstantinou@gmail.com>
2025-10-01 21:35:46 +0200

    update_to_standalone

--- MatBaseX-v1.3/Examples/Examples_PES_NLayerStackModelling.m
+++ MatBaseX-v1.3/Examples/Examples_PES_NLayerStackModelling.m
@@ -1,7 +1,21 @@
 close all; clear all;
 
-%% Modeller 02 - Uniform and Varying Concentration Layer Stack
-%% 1    :   Plotting an arbitrary stack
+%% 1    :   Uniform and Varying Concentration Layer Stack
+%% 1.1    :   Constant Experimental Geometry
+close all; clear all;
+lyr_mat         = {"SiO2", "Si"};
+lyr_ele         = {"Si", "Si"};
+lyr_cls         = {"2p3", "2p3"};
+lyr_thick       = {2.0, Inf};
+lyr_density     = {[], []};
+lyr_exclude     = {};
+hv              = 5000;
+theta           = 45;
+phi             = 45;
+simul_pes_nlayer_intensity(...
+    lyr_mat, lyr_ele, lyr_cls, lyr_thick, lyr_density, lyr_exclude,...
+    hv, theta,phi,[],[],[],1);
+%% 1.2    :   Photon Energy Dependence
 close all; clear all;
 lyr_mat         = {"SiO2", "Si"};
 lyr_ele         = {"Si", "Si"};
@@ -13,8 +27,7 @@ hv              = 1500:100:10000;
 simul_pes_nlayer_intensity(...
     lyr_mat, lyr_ele, lyr_cls, lyr_thick, lyr_density, lyr_exclude,...
     hv, [],[],[],[],[],1);
-
-%% 1    :   Plotting an arbitrary stack
+%% 1.3    :   Emission Angle Dependence
 close all; clear all;
 lyr_mat         = {"SiO2", "Si"};
 lyr_ele         = {"Si", "Si"};
@@ -28,8 +41,7 @@ phi             = 0;
 simul_pes_nlayer_intensity(...
     lyr_mat, lyr_ele, lyr_cls, lyr_thick, lyr_density, lyr_exclude,...
     hv, theta,[],[],[],[],1);
-
-%% 1    :   Plotting an arbitrary stack
+%% 1.4    :   Azimuthal Dependence
 close all; clear all;
 lyr_mat         = {"SiO2", "Si"};
 lyr_ele         = {"Si", "Si"};
@@ -44,22 +56,9 @@ simul_pes_nlayer_intensity(...
     lyr_mat, lyr_ele, lyr_cls, lyr_thick, lyr_density, lyr_exclude,...
     hv, theta,phi,[],[],[],1);
 
-%% 1    :   Plotting an arbitrary stack
-close all; clear all;
-lyr_mat         = {"SiO2", "Si"};
-lyr_ele         = {"Si", "Si"};
-lyr_cls         = {"2p3", "2p3"};
-lyr_thick       = {2.0, Inf};
-lyr_density     = {[], []};
-lyr_exclude     = {};
-hv              = 5000;
-theta           = 45;
-phi             = 45;
-simul_pes_nlayer_intensity(...
-    lyr_mat, lyr_ele, lyr_cls, lyr_thick, lyr_density, lyr_exclude,...
-    hv, theta,phi,[],[],[],1);
+%% 2        :    Multilayer Simulations
+%% 2.1    :   Photon Energy Dependence
 
-%% 3        :    Varying concentration layer
 close all;
 lyr_mat         = {'SiO2', 'Al', 'Al','In', 'InAs'};
 lyr_ele         = {'Si','Al','Al','In','In'};
@@ -73,8 +72,7 @@ phi             = 0;
 simul_pes_nlayer_intensity(...
     lyr_mat, lyr_ele, lyr_cls, lyr_thick, lyr_density, lyr_exclude,...
     hv, theta,phi,[],[],[],1);
-
-%% 3        :    Varying concentration layer
+%% 2.2    :   Emission Angle Dependence
 close all;
 lyr_mat         = {'SiO2', 'Al', 'Al','In', 'InAs'};
 lyr_ele         = {'Si','Al','Al','In','In'};
@@ -89,17 +87,16 @@ simul_pes_nlayer_intensity(...
     lyr_mat, lyr_ele, lyr_cls, lyr_thick, lyr_density, lyr_exclude,...
     hv, theta,phi,[],[],[],1);
 
-%%
+%% 3    :   Calculating Information Depth
+close all;
 eK = 1000;
 theta = 0;
 lyr_mat         = {'SiO2', 'U', 'Pb','Al', 'InAs'};
 lyr_thick       = {3, 6, 5, 5, Inf};
-formalism_imfp = "TPP2m";
-info_depth = simul_pes_nlayer_info_depth(lyr_mat, lyr_thick, eK, theta, formalism_imfp, 1);
+formalism_imfp  = "TPP2m";
+info_depth      = simul_pes_nlayer_info_depth(lyr_mat, lyr_thick, eK, theta, formalism_imfp, 1);
 
-
-
-%%
+%% 4    :   Plotting Sample Stack
 close all;
 lyr_mat         = {'SiO2', 'U', 'Pb','Al2O3', 'InAs'};
 lyr_thick       = {3, 6, 5, 5, Inf};
